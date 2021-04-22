0e2bc64f3e20d6312407a00d8c3ed377208320d3
Tubiziim <62650402+Tuba-Akbas@users.noreply.github.com>
2021-04-22 19:38:50 +0300

    v0.4
    Buton Eklendi

--- BorsaUygulama/Borsa/GirisFormu.Designer.cs
+++ BorsaUygulama/Borsa/GirisFormu.Designer.cs
@@ -44,6 +44,7 @@ namespace Borsa
             this.panel1 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -92,6 +93,7 @@ namespace Borsa
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.btnKayitOl);
             this.panel2.Controls.Add(this.txtSifre);
             this.panel2.Controls.Add(this.label3);
@@ -185,6 +187,15 @@ namespace Borsa
             this.label5.TabIndex = 15;
             this.label5.Text = "Admin Giris";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // GirisFormu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -222,6 +233,7 @@ namespace Borsa
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- BorsaUygulama/Borsa/KullaniciManager.cs
+++ BorsaUygulama/Borsa/KullaniciManager.cs
@@ -27,7 +27,7 @@ namespace Borsa
         }
         public Boolean KullaniciAdKontrol(KullaniciTbl kullanici)
         {
-            return false;
+            return true;
         }
     }
 }
